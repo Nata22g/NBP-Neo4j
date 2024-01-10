768ef1df9e1f85a6bc46c06177f8cb62a7d5f05d
Selena Dimitrijević <selenadimitrijevic@elfak.rs>
2024-01-10 15:43:08 +0100

    tim CRUD osnovno

---
+++ tim.js
@@ -0,0 +1,149 @@
+import { session } from './index.js'
+
+//prikazi radnike - GET
+export const prikaziSveTimove = async(req, res) => {
+    try {
+        let timovi = []
+        const query = 'MATCH (t:Tim) RETURN t'
+        await session
+                .run(query)
+                .then(result => {
+                    result.records.forEach(record => {
+                        timovi.push(record._fields[0].properties)
+                    });
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+
+        return res.status(200).json(timovi)
+
+    } catch(err) {
+        return res.status(500).json(err)
+    }
+}
+
+//dodaj radnika - POST
+export const dodajTim = async(req, res) => {
+    try {
+        let dodatiTim = null
+        let timPostoji = false
+        const query = `MATCH (t:Tim {Naziv: '${req.body.naziv}'}) RETURN t`
+
+        await session
+                .run(query)
+                .then(result => {
+                    if(result.records.length !== 0) {
+                        timPostoji = true
+                    }
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+
+        if (!timPostoji) {
+            const query1 = `CREATE (t:Tim 
+                            {
+                                Naziv: '${req.body.naziv}', 
+                                Status: '${req.body.status}'
+                            }) 
+                            RETURN t`
+
+            await session
+                    .run(query1)
+                    .then(result => {
+                        dodatiTim = result.records[0]._fields[0].properties
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    })
+
+            return res.status(200).json(dodatiTim)
+        } else {
+            return res.status(400).json('Tim sa unetim nazivom vec postoji!')
+        }
+        
+
+    } catch(err) {
+        return res.status(500).json(err)
+    }
+}
+
+//obrisi radnika - DELETE
+export const obrisiTim = async(req, res) => {
+    try {
+        let timPostoji = false
+        const query = `MATCH (t:Tim {Naziv: '${req.body.naziv}'}) RETURN t`
+
+        await session
+                .run(query)
+                .then(result => {
+                    if(result.records.length !== 0) {
+                        timPostoji = true
+                    }
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+
+
+        if (timPostoji) {
+            const query1 = `MATCH (t:Tim {Naziv: '${req.body.naziv}'}) DETACH DELETE t`
+            await session
+                    .run(query1)
+                    .then(result => {
+                        console.log(result)
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    })
+
+            return res.status(200).json('Tim uspesno obrisan')
+        } else {
+            return res.status(404).json('Tim sa unetim nazivom ne postoji!')
+        }
+
+    } catch (err) {
+        return res.status(500).json(err)
+    }
+}
+
+//izmeni radnika - PUT
+export const izmeniTim = async(req, res) => {
+    try {
+        let timPostoji = false
+        const query = `MATCH (t:Tim {Naziv: '${req.body.naziv}'}) RETURN t`
+
+        await session
+                .run(query)
+                .then(result => {
+                    if(result.records.length !== 0) {
+                        timPostoji = true
+                    }
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+
+
+        if (timPostoji) {
+            let izmenjeniTim = null
+            const query1 = `MATCH (t:Tim {Naziv: '${req.body.naziv}'}) SET t.Status = '${req.body.status}' RETURN t`
+            await session
+                    .run(query1)
+                    .then(result => {
+                        izmenjeniTim = result.records[0]._fields[0].properties
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    })
+
+            return res.status(200).json(izmenjeniTim)
+        } else {
+            return res.status(404).json('Tim sa unetim nazivom ne postoji!')
+        }
+
+    } catch (err) {
+        return res.status(500).json(err)
+    }
+}
